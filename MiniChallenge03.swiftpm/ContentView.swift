ef0ef1b0877edd8cdc2f4be4d8cf68e2093bb4ab
João Ângelo <134436082+joaoangelo76@users.noreply.github.com>
2023-12-11 17:54:28 -0300

    Commit test.

---
+++ MiniChallenge03.swiftpm/ContentView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
